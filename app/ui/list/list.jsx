b6cf82460c348c66e301f431f39b1f7c6657ada6
Oliver Macedo <oli932300102@gmail.com>
2024-03-27 13:11:23 -0500

    add input, lind and list component

---
+++ app/ui/list/list.jsx
@@ -0,0 +1,20 @@
+import { classes } from '@/app/utils/style';
+import styles from './list.module.css';
+
+export const List = ({ ordered, children, className, ...rest }) => {
+  const Element = ordered ? 'ol' : 'ul';
+
+  return (
+    <Element className={classes(styles.list, className)} {...rest}>
+      {children}
+    </Element>
+  );
+};
+
+export const ListItem = ({ children, ...rest }) => {
+  return (
+    <li className={styles.item} {...rest}>
+      {children}
+    </li>
+  );
+};
